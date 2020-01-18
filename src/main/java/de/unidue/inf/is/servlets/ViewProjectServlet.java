0be192208996f0199b4f3553476cd2f9e8b12985
9Sefa9 <sefagoevercin@hotmail.de>
2020-01-18 01:10:54 +0100

    Vorlagen vorbereitet für die nächsten *.ftl Dateien

---
+++ src/main/java/de/unidue/inf/is/servlets/ViewProjectServlet.java
@@ -0,0 +1,22 @@
+package de.unidue.inf.is.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import de.unidue.inf.is.utils.DBUtil;
+
+
+public final class ViewProjectServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+                    throws ServletException, IOException {
+  
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/view_project.ftl
@@ -0,0 +1,6 @@
+<html>
+<head><title>ProjectFunder</title>
+<link rel="stylesheet" type="text/css" href="/css/view_project.css">
+<body>
+</body>
+</html>
